e1fd69a3201acaade1c54caada5528c08738f222
Shichang Tang <tangshch@shanghaitech.edu.cn>
2018-11-18 16:09:00 +0800

    Create fid.py

---
+++ fid.py
@@ -0,0 +1,74 @@
+'''
+From https://github.com/tsc2017/frechet-inception-distance
+
+Usage:
+    Call get_fid(images1, images2)
+Args:
+    images1, images2: Numpy arrays with values ranging from 0 to 255 and shape in the form [N, 3, HEIGHT, WIDTH] where N, HEIGHT and WIDTH can be arbitrary. 
+    dtype of the images is recommended to be np.uint8 to save CPU memory.
+Returns:
+    Frechet Inception Distance between the two image distributions.
+'''
+
+import tensorflow as tf
+import os, sys
+import functools
+import numpy as np
+import time
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import functional_ops
+tfgan = tf.contrib.gan
+
+session = tf.InteractiveSession()
+
+# A smaller BATCH_SIZE reduces GPU memory usage, but at the cost of a slight slowdown
+BATCH_SIZE = 64
+
+# Run images through Inception.
+inception_images = tf.placeholder(tf.float32, [BATCH_SIZE, 3, None, None])
+activations1 = tf.placeholder(tf.float32, [None, None], name = 'activations1')
+activations2 = tf.placeholder(tf.float32, [None, None], name = 'activations2')
+fcd = tfgan.eval.frechet_classifier_distance_from_activations(activations1, activations2)
+
+def inception_activations(images = inception_images, num_splits = 1):
+    images = tf.transpose(images, [0, 2, 3, 1])
+    size = 299
+    images = tf.image.resize_bilinear(images, [size, size])
+    generated_images_list = array_ops.split(images, num_or_size_splits = num_splits)
+    activations = functional_ops.map_fn(
+        fn = functools.partial(tfgan.eval.run_inception, output_tensor = 'pool_3:0'),
+        elems = array_ops.stack(generated_images_list),
+        parallel_iterations = 1,
+        back_prop = False,
+        swap_memory = True,
+        name = 'RunClassifier')
+    activations = array_ops.concat(array_ops.unstack(activations), 0)
+    return activations
+
+activations =inception_activations()
+
+def get_inception_activations(inps, softmax=False):
+    n_batches = inps.shape[0]//BATCH_SIZE
+    preds = np.zeros([n_batches * BATCH_SIZE, 2048], dtype = np.float32)
+    for i in range(n_batches):
+        inp = inps[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] / 255. * 2 - 1
+        preds[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] = activations.eval({inception_images: inp})
+    return preds
+
+def get_fid(images1, images2):
+    assert(type(images1) == np.ndarray)
+    assert(len(images1.shape) == 4)
+    assert(images1.shape[1] == 3)
+    assert(np.min(images1[0]) >= 0 and np.max(images1[0]) > 10), 'Image values should be in the range [0, 255]'
+    assert(type(images2) == np.ndarray)
+    assert(len(images2.shape) == 4)
+    assert(images2.shape[1] == 3)
+    assert(np.min(images2[0]) >= 0 and np.max(images2[0]) > 10), 'Image values should be in the range [0, 255]'
+    assert(images1.shape == images2.shape), 'The two numpy arrays must have the same shape'
+    print('Calculating FID with %i images from each distribution' % (images1.shape[0]))
+    start_time = time.time()
+    preds1 = get_inception_activations(images1)
+    preds2 = get_inception_activations(images2)
+    fid = fcd.eval(feed_dict = {activations1: preds1, activations2: preds2})
+    print('FID calculation time: %f s' % (time.time() - start_time))
+    return fid
