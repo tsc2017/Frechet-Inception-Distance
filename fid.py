d8eb29dbe1ff3a4cc170cde69c37378e569386b2
Shichang Tang <tangshch@shanghaitech.edu.cn>
2019-08-07 15:12:20 +0800

    Compatibility with Tensorflow 1.14
    support small samples

--- fid.py
+++ fid.py
@@ -12,31 +12,29 @@ Returns:
 '''
 
 import tensorflow as tf
-import os, sys
+import os
 import functools
 import numpy as np
 import time
 from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import functional_ops
 tfgan = tf.contrib.gan
 
-session = tf.InteractiveSession()
-
+session=tf.compat.v1.InteractiveSession()
 # A smaller BATCH_SIZE reduces GPU memory usage, but at the cost of a slight slowdown
 BATCH_SIZE = 64
 
 # Run images through Inception.
-inception_images = tf.placeholder(tf.float32, [BATCH_SIZE, 3, None, None])
-activations1 = tf.placeholder(tf.float32, [None, None], name = 'activations1')
-activations2 = tf.placeholder(tf.float32, [None, None], name = 'activations2')
+inception_images = tf.compat.v1.placeholder(tf.float32, [None, 3, None, None])
+activations1 = tf.compat.v1.placeholder(tf.float32, [None, None], name = 'activations1')
+activations2 = tf.compat.v1.placeholder(tf.float32, [None, None], name = 'activations2')
 fcd = tfgan.eval.frechet_classifier_distance_from_activations(activations1, activations2)
 
 def inception_activations(images = inception_images, num_splits = 1):
     images = tf.transpose(images, [0, 2, 3, 1])
     size = 299
-    images = tf.image.resize_bilinear(images, [size, size])
+    images = tf.compat.v1.image.resize_bilinear(images, [size, size])
     generated_images_list = array_ops.split(images, num_or_size_splits = num_splits)
-    activations = functional_ops.map_fn(
+    activations = tf.map_fn(
         fn = functools.partial(tfgan.eval.run_inception, output_tensor = 'pool_3:0'),
         elems = array_ops.stack(generated_images_list),
         parallel_iterations = 1,
@@ -49,15 +47,15 @@ def inception_activations(images = inception_images, num_splits = 1):
 activations =inception_activations()
 
 def get_inception_activations(inps):
-    n_batches = inps.shape[0]//BATCH_SIZE
-    act = np.zeros([n_batches * BATCH_SIZE, 2048], dtype = np.float32)
+    n_batches = int(np.ceil(float(inps.shape[0]) / BATCH_SIZE))
+    act = np.zeros([inps.shape[0], 2048], dtype = np.float32)
     for i in range(n_batches):
-        inp = inps[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] / 255. * 2 - 1
-        act[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] = activations.eval(feed_dict = {inception_images: inp})
+        inp = inps[i * BATCH_SIZE : (i + 1) * BATCH_SIZE] / 255. * 2 - 1
+        act[i * BATCH_SIZE : i * BATCH_SIZE + min(BATCH_SIZE, inp.shape[0])] = session.run(activations, feed_dict = {inception_images: inp})
     return act
 
 def activations2distance(act1, act2):
-     return fcd.eval(feed_dict = {activations1: act1, activations2: act2})
+     return session.run(fcd, feed_dict = {activations1: act1, activations2: act2})
         
 def get_fid(images1, images2):
     assert(type(images1) == np.ndarray)
